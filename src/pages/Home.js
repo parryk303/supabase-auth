67134c57b7ffa7093a924005db62ebc5bf1da12d
Kyle Parry <kyle.parry.303@gmail.com>
2022-06-17 22:17:10 -0600

    Add axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { supabase } from "../supabaseClient";
+import axios from 'axios';
 
 export default function Home() {
+  const [get, setGet] = useState(true)
+  const [users, setUsers] = useState()
   const history = useHistory();
 
   useEffect(() => {
     if (supabase.auth.user() === null) {
       history.replace("/login");
     }
-  }, []);
+  }, [history]);
+
+  useEffect(() => {
+    async function makeGetRequest() {
+
+      let res = await axios.get('http://18.222.162.221:3000/todos');
+
+      let data = res.data;
+      setUsers(data)
+      console.log(data);
+    }
+
+
+    if (get) {
+      makeGetRequest();
+      setGet(false)
+    }
+  }, [get]);
 
   return (
     <div className="container">
@@ -17,7 +37,7 @@ export default function Home() {
         <div className="col-xs-12 col-lg-8">
           <div className="card-title text-center mb-3">
             <h4>Home Page</h4>
-            <p>You have sucessfully logged in!</p>
+            <p>Yo</p>
           </div>
         </div>
       </div>
